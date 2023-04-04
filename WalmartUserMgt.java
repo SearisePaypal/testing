65b40f7731e974d8aac860f74119e8d02a433624
Ayomide012023 <Omotennyayo@outlook.com>
2023-04-04 05:49:23 -0400

    4th commit of the walmart black friday app modified

--- WalmartUserMgt.java
+++ WalmartUserMgt.java
@@ -3,4 +3,8 @@ We are are building Back to School application for our client Walmart
 We missed a line of code here, fixed by Prof Yinka
 The project about start
 Here is the operation team about to start development
-Here is the Black Friday App
+Here is the Black Friday App 
+The Walmart Black friday App brings latest shopping deals
+The Walmart Black Friday App were you can create content
+The Walmart Black friday App that Run giveaway
+With walmart Black friday App you can save big
