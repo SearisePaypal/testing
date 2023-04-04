886ba420a75e92a543c111b3c8472627cbcc0b0d
Ayomide012023 <Omotennyayo@outlook.com>
2023-04-04 04:27:14 -0400

    1st commit of the Walmart Black Friday app

---
+++ WalmartUserMgt.java
@@ -0,0 +1,5 @@
+#This Walmart New Project
+We are are building Back to School application for our client Walmart
+We missed a line of code here, fixed by Prof Yinka
+The project about start
+Here is the operation team about to start development
